99ccaeb01aff64e9c4a6ec1cd1d0dc408950565e
y-emam <YasserEmam2002@gmail.com>
2024-07-22 18:31:32 +0300

    starting to go for DB

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,17 +1,28 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
-const Product = ({ params }) => {
+// export const metadata = {
+//   title: ""
+// }
+
+const Product = ({ product }) => {
   return (
     <section className="text-white">
-      <h1>Product {params.id} Name</h1>
-      {/* <Image /> */}
-      <p>Product Description</p>
-      <p>Product Price</p>
-      <button>Add to Cart</button>
+      <h1>{product.name}</h1>
+      <Image
+        width={product.width}
+        height={product.height}
+        src={product.imageUri}
+        alt={product.alt}
+      />
+      <p>{product.description}</p>
+      <p>${product.price}</p>
+      <button onClick={() => {}}>Buy</button>
     </section>
   );
+  // todo: check if you will change buy to add to cart or no
 };
 
 export default Product;
