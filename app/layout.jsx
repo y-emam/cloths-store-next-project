9c64da95269be3b78ffc25c8c9193964c02c62de
y-emam <YasserEmam2002@gmail.com>
2024-07-22 18:23:54 +0300

    changed the browser tab icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/icons/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <div className="main">
           <div className="gradient" />
