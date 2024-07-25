4c9a3b2a34e4754c331fb4b5523bbc06133270a4
y-emam <YasserEmam2002@gmail.com>
2024-07-25 17:07:49 +0300

    added payment to stripe

---
+++ app/payment/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import CheckoutPage from "@/components/CheckoutPage";
+import convertToSubCurrency from "@/lib/convertToSubCurrency";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useParams } from "next/navigation";
+import React from "react";
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined");
+}
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
+const PaymentPage = () => {
+  const amount = 49.99;
+
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-black">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Yasuo</h1>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold"> ${amount}</span>
+        </h2>
+      </div>
+
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: convertToSubCurrency(amount),
+          currency: "usd",
+        }}
+      >
+        <CheckoutPage amount={amount} />
+      </Elements>
+    </main>
+  );
+};
+
+export default PaymentPage;
